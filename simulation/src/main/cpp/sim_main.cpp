ec7ba7f9ad7b2ecd227fcceae598bd8563de6fb5
Jaci Brunning <jaci.brunning@gmail.com>
2018-09-28 22:52:06 +0800

    Add comment to new pointer allocs

--- simulation/src/main/cpp/sim_main.cpp
+++ simulation/src/main/cpp/sim_main.cpp
@@ -9,6 +9,9 @@
 
 static void pwm_init_callback(const char* name, void* param, const struct HAL_Value* value) {
   int pwm = (int)param;
+  // You should _never_ allocate a new object without deleting / freeing it at some point, but 
+  // we purposefully want these to outlive the scope of run() since they own threads, and we want
+  // them to run forever until the simulation is ended, at which point the OS picks up the mess.
   (new sim_motor_window(pwm))->start();
 }
 
